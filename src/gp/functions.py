68542ae5d2403383b9e29459f87bc4024dfa78d8
roman <roman.kalkreuth@rwth-aachen.de>
2025-01-22 12:59:48 +0100

    functions module added to create functions sets for SR, LS and RL

---
+++ src/gp/functions.py
@@ -0,0 +1,20 @@
+from multiprocessing.process import ORIGINAL_DIR
+
+from src.tinyverse import Function
+import operator
+
+def pdiv(x, y):
+    return x / y if y > 0 else 1.0
+
+# Arithmetic Functions
+ADD = Function(2, 'ADD', operator.add)
+SUB = Function(2, 'SUB', operator.sub)
+MUL = Function(2, 'MUL', operator.mul)
+DIV = Function(2, 'DIV', pdiv)
+
+# Logical Functions
+AND = Function(2, 'AND', operator.and_)
+OR  = Function(2, 'OR', operator.or_)
+NAND = Function(2, 'NAND', operator.not_(operator.and_))
+NOR = Function(2, 'Nor', operator.not_(operator.or_))
+NOT = Function(1, 'NOR', operator.not_)
