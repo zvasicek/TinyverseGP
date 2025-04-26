a17cc81167ae89688d37300990d94e49b4c20504
Giovanni Squillero <giovanni.squillero@polito.it>
2025-04-26 17:26:56 +0200

    Add LGP evaluation and expression

---
+++ examples/symbolic_regression/test_lgp_sr.py
@@ -0,0 +1,71 @@
+"""
+Example module to test LGP with symbolic regression problems.
+
+Attempts to evolve a solution for the Koza-1 benchmkark which is
+a quartic polynomial: x^4 + x^3 + x^2 + x
+
+The problem is specified in the following paper:
+https://dl.acm.org/doi/10.1145/2330163.2330273
+
+Please note: This benchmark is nowadays considered a toy problem and
+no serious benchmark. It only serves as an example for SR as an application
+domain for TinyverseGP:
+"""
+
+from src.gp.tiny_lgp import *
+from src.gp.problem import BlackBox
+from src.benchmark.symbolic_regression.sr_benchmark import SRBenchmark
+from src.gp.functions import *
+from src.gp.loss import *
+from src.gp.tinyverse import Var, Const
+
+try:
+    from icecream import ic
+except ModuleNotFoundError:
+    ic = lambda *args, **kwargs: None
+
+ARITHMETIC_FUNCTIONS = [ADD, SUB, MUL, DIV]
+
+
+def main():
+    functions = ARITHMETIC_FUNCTIONS
+    terminals = [Var(0), Const(1)]
+
+    config = LGPConfig(
+        num_jobs=1,
+        max_generations=100,
+        stopping_criteria=1e-6,
+        minimizing_fitness=True,
+        ideal_fitness=1e-6,
+        silent_algorithm=False,
+        silent_evolver=False,
+        minimalistic_output=True,
+        report_interval=1,
+        max_time=60,
+        num_inputs=1,
+        num_registers=8,
+    )
+
+    hyperparameters = LGPHyperparameters(
+        mu=10,
+        lambda_=30,
+    )
+
+    loss = absolute_distance
+    data, actual = SRBenchmark().generate('KOZA3')
+    ic(data)
+    ic(actual)
+
+    # problem = BlackBox(data, actual, loss, 1e-6, True)
+    lgp = TinyLGP()
+    # cgp.evolve()
+
+    genome = [
+        Instruction('R0', ADD, ('I0', 0)),
+    ]
+    ic(lgp.predict(genome, data))
+    ic(lgp.expression(genome))
+
+
+if __name__ == '__main__':
+    main()
--- src/gp/tiny_lgp.py
+++ src/gp/tiny_lgp.py
@@ -2,16 +2,146 @@
 TinyLGP: A minimalistic implementation of Linear Genetic Programming for
          TinyverseGP.
 
-         Genome representation: 
-         Mutation operator: 
-         Search algorithm: 
+         Genome representation:
+         Mutation operator:
+         Search algorithm:
 """
+
+from typing import Sequence, Any
 
 import math
 import random
 import time
 from dataclasses import dataclass
+from collections import namedtuple
 from enum import Enum
 from src.gp.tinyverse import GPModel, Hyperparameters, GPConfig, Var
 from src.gp.problem import Problem
 
+try:
+    from icecream import ic
+except ModuleNotFoundError:
+    ic = lambda *args, **kwargs: None
+
+
+@dataclass
+class LGPHyperparameters(Hyperparameters):
+    """
+    Specialized hyperparameter configuration space for LGP.
+    """
+
+    mu: int
+    lambda_: int
+    # levels_back: int
+    # strict_selection: bool
+    # mutation_rate: float = None
+    # mutation_rate_genes: int = None
+
+    def __init__(self, *, mu=10, lambda_=20, **kwargs):
+        self.mu = mu
+        self.lambda_ = lambda_
+        super().__init__(**kwargs)
+
+
+@dataclass
+class LGPConfig(GPConfig):
+    """
+    Specialized GP configuration that is needed to run LGP.
+    """
+
+    num_inputs: int
+    num_registers: int
+    # num_function_nodes: int
+    # num_functions: int
+    # max_arity: int
+    max_time: int
+    report_every_improvement: bool = False
+
+    def __init__(
+        self,
+        *,
+        num_inputs,
+        num_outputs=1,
+        num_registers=16,
+        report_every_improvement=True,
+        **kwargs,
+    ):
+        self.num_inputs = num_inputs
+        self.num_registers = num_registers
+        self.report_every_improvement = report_every_improvement
+        super().__init__(**kwargs, num_outputs=num_outputs)
+
+
+Instruction = namedtuple('Instruction', ['dest', 'operator', 'operands'])
+
+
+class LGPIndividual:
+    """
+    Class that is used to represent a LGP individual.
+    Formally ...
+    """
+
+    def __init__(self, genome: Sequence[Instruction], fitness: float | None = None):
+        # TODO: Use multiple values for fitness
+        self.genome = tuple(genome)
+        self.fitness = fitness
+
+
+class TinyLGP(GPModel):
+    """
+    Main class of the tiny CGP module that derives from GPModel and
+    implements all related fundamental mechanisms tun run CGP.
+    """
+
+    num_evaluations: int
+    population: list[LGPIndividual]
+
+    def __init__(self):
+        self.num_evaluations = 0
+
+    def evolve(self) -> Any: ...
+
+    def predict(self, genome: Sequence[Instruction], observations: list) -> list:
+        predictions = list()
+        for observation in observations:
+            registers = {f'R{n}': 0.0 for n in range(10)} | {
+                f'I{n}': v for n, v in enumerate(observation)
+            }
+
+            for instruction in genome:
+                # if the operand is the name of a register, then use the value in the register, otherwise use it diretly
+                registers[instruction.dest] = instruction.operator(
+                    *[registers[r] if r in registers else r for r in instruction.operands]
+                )
+            predictions.append(registers['R0'])
+        return predictions
+
+    def expression(self, genome: Sequence[Instruction]) -> Any:
+        return '\n'.join(
+            f'''{i.dest} = {i.operator.name}({', '.join(str(_) for _ in i.operands)})'''
+            for i in genome
+        )
+
+    def fitness(self, individual: LGPIndividual) -> float:
+        """
+        Return the fitness value of an individual.
+        """
+        return individual.fitness
+
+    def evaluate(self) -> (LGPIndividual, bool):
+        """
+        Evaluates the population.
+
+        :returns: the best solution discovered in the population
+        """
+        best = None
+
+    def evaluate_individual(self, genome: Sequence[Instruction]) -> float:
+        """
+        Evaluates an individual against the problem.
+
+        :param genome: the genome of an individual
+        :return: fitness of the individual
+        """
+        self.num_evaluations += 1
+        return self.problem.evaluate(genome, self)
