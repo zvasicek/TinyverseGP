4910f196d5c1e4443295db8fe7708378fe8a9af9
roman <roman.kalkreuth@rwth-aachen.de>
2025-06-18 22:23:28 +0200

    Minor issues with config and hyperparameter datalasses fixed

---
+++ examples/checkpointing/test_cgp_checkpointing.py
@@ -0,0 +1,58 @@
+from src.gp.tiny_cgp import *
+from src.gp.problem import BlackBox
+from src.benchmark.symbolic_regression.sr_benchmark import SRBenchmark
+from src.gp.functions import *
+from src.gp.loss import *
+from src.gp.tinyverse import Var, Const, Checkpointer, Hyperparameters
+
+functions = [ADD, SUB, MUL, DIV]
+terminals = [Var(0), Const(1)]
+
+config = CGPConfig(
+    global_seed=13,
+    num_jobs=1,
+    max_generations=1000,
+    stopping_criteria=1e-6,
+    minimizing_fitness=True,
+    ideal_fitness=1e-6,
+    silent_algorithm=False,
+    silent_evolver=False,
+    minimalistic_output=True,
+    num_functions=len(functions),
+    max_arity=2,
+    num_inputs=1,
+    num_outputs=1,
+    num_function_nodes=10,
+    report_interval=100,
+    max_time=60,
+    checkpoint_interval=100,
+    checkpoint_dir="checkpoints",
+    experiment_name="my_experiment"
+)
+
+hyperparameters = CGPHyperparameters(
+    mu=1,
+    lmbda=32,
+    population_size=33,
+    levels_back=len(terminals),
+    mutation_rate=0.1,
+    strict_selection=True
+)
+
+loss = absolute_distance
+benchmark = SRBenchmark()
+data, actual = benchmark.generate('KOZA3')
+
+problem = BlackBox(data, actual, loss, config.ideal_fitness, config.minimizing_fitness)
+
+cgp = TinyCGP(functions, terminals, config, hyperparameters)
+cgp.evolve(problem)
+
+cp = Checkpointer(config, hyperparameters)
+checkpoint = cp.load("checkpoints/my_experiment/checkpoint_gen_500.json")
+
+hyperparameters = CGPHyperparameters.from_dict(checkpoint["hyperparameters"])
+config = CGPConfig.from_dict(checkpoint["config"])
+
+cgp = TinyCGP(functions, terminals, config, hyperparameters)
+cgp.resume(checkpoint, problem)
